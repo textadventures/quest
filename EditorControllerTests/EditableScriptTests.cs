4bfa880187663f37044754b2660c2c5b0526d71c
Alex Warren <alex@axeuk.com>
2010-12-04 19:30:59 +0000

    Rearrange unit tests a bit
    git-svn-id: https://quest.svn.codeplex.com/svn/trunk@3939 4a90e977-6436-4932-9605-20e10c2b6fb3

--- EditorControllerTests/EditableScriptTests.cs
+++ EditorControllerTests/EditableScriptTests.cs
@@ -8,56 +8,35 @@ using AxeSoftware.Quest;
 namespace EditorControllerTests
 {
     [TestClass]
-    public class EditableScriptTests
+    public class EditableScriptTests : EditorControllerTestBase
     {
-        private EditorController m_controller;
-        private EditorTreeData m_tree = new EditorTreeData();
-
-        [TestInitialize]
-        public void Init()
-        {
-            m_controller = new EditorController();
-            m_controller.ClearTree += new EditorController.VoidHandler(m_controller_ClearTree);
-            m_controller.BeginTreeUpdate += new EditorController.VoidHandler(m_controller_BeginTreeUpdate);
-            m_controller.EndTreeUpdate += new EditorController.VoidHandler(m_controller_EndTreeUpdate);
-            m_controller.AddedNode += new EditorController.AddedNodeHandler(m_controller_AddedNode);
-            m_controller.Initialise(@"..\..\..\EditorControllerTests\test.aslx");
-        }
-
-        void m_controller_ClearTree()
-        {
-            m_tree.Clear();
-        }
-
-        void m_controller_BeginTreeUpdate()
-        {
-            m_tree.BeginUpdate();
-        }
-
-        void m_controller_EndTreeUpdate()
-        {
-            m_tree.EndUpdate();
-        }
-
-        void m_controller_AddedNode(string key, string text, string parent, System.Drawing.Color? foreColor, System.Drawing.Color? backColor)
-        {
-            m_tree.Add(key, text, parent);
-        }
-
         [TestMethod]
-        public void CreateScript()
+        public void TestUndoRedo()
         {
-            EditableScripts newScripts = m_controller.CreateNewEditableScripts("game", "somescript", "msg (\"hello\")");
+            Assert.AreEqual(0, UndoList.Count);
+            Assert.AreEqual(0, RedoList.Count);
+            EditableScripts newScripts = Controller.CreateNewEditableScripts("game", "somescript", "msg (\"hello\")");
             Assert.AreEqual(1, newScripts.Scripts.Count());
+            Assert.AreEqual(1, UndoList.Count);
+            Assert.AreEqual(0, RedoList.Count);
             IEditableScript script = newScripts[0];
             Assert.AreEqual("Print \"hello\"", script.DisplayString());
-            m_controller.StartTransaction("Change msg script value");
+            Controller.StartTransaction("Change msg script value");
             script.SetParameter(0, "\"new value\"");
-            m_controller.EndTransaction();
+            Controller.EndTransaction();
+            Assert.AreEqual(2, UndoList.Count);
+            Assert.AreEqual(0, RedoList.Count);
+            Assert.AreEqual("Change msg script value", UndoList[0]);
             Assert.AreEqual("Print \"new value\"", script.DisplayString());
-            m_controller.Undo();
+            Controller.Undo();
+            Assert.AreEqual(1, UndoList.Count);
+            Assert.AreEqual(1, RedoList.Count);
+            Assert.AreEqual("Change msg script value", RedoList[0]);
             Assert.AreEqual("Print \"hello\"", script.DisplayString());
-            m_controller.Redo();
+            Controller.Redo();
+            Assert.AreEqual(2, UndoList.Count);
+            Assert.AreEqual(0, RedoList.Count);
+            Assert.AreEqual("Change msg script value", UndoList[0]);
             Assert.AreEqual("Print \"new value\"", script.DisplayString());
         }
     }
---
+++ EditorControllerTests/EditorControllerTestBase.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AxeSoftware.Quest;
+
+namespace EditorControllerTests
+{
+    public abstract class EditorControllerTestBase
+    {
+        private EditorController m_controller;
+        private EditorTreeData m_tree = new EditorTreeData();
+        private List<string> m_undoList = new List<string>();
+        private List<string> m_redoList = new List<string>();
+
+        protected EditorController Controller
+        {
+            get { return m_controller; }
+        }
+
+        protected List<string> UndoList
+        {
+            get { return m_undoList; }
+        }
+
+        protected List<string> RedoList
+        {
+            get { return m_redoList; }
+        }
+
+        [TestInitialize]
+        public void Init()
+        {
+            m_controller = new EditorController();
+            m_controller.ClearTree += new EditorController.VoidHandler(m_controller_ClearTree);
+            m_controller.BeginTreeUpdate += new EditorController.VoidHandler(m_controller_BeginTreeUpdate);
+            m_controller.EndTreeUpdate += new EditorController.VoidHandler(m_controller_EndTreeUpdate);
+            m_controller.AddedNode += new EditorController.AddedNodeHandler(m_controller_AddedNode);
+            m_controller.UndoListUpdated += new EventHandler<EditorController.UpdateUndoListEventArgs>(m_controller_UndoListUpdated);
+            m_controller.RedoListUpdated += new EventHandler<EditorController.UpdateUndoListEventArgs>(m_controller_RedoListUpdated);
+            m_controller.Initialise(@"..\..\..\EditorControllerTests\test.aslx");
+        }
+
+        void m_controller_ClearTree()
+        {
+            m_tree.Clear();
+        }
+
+        void m_controller_BeginTreeUpdate()
+        {
+            m_tree.BeginUpdate();
+        }
+
+        void m_controller_EndTreeUpdate()
+        {
+            m_tree.EndUpdate();
+        }
+
+        void m_controller_AddedNode(string key, string text, string parent, System.Drawing.Color? foreColor, System.Drawing.Color? backColor)
+        {
+            m_tree.Add(key, text, parent);
+        }
+
+        void m_controller_UndoListUpdated(object sender, EditorController.UpdateUndoListEventArgs e)
+        {
+            m_undoList = new List<string>(e.UndoList);
+        }
+
+        void m_controller_RedoListUpdated(object sender, EditorController.UpdateUndoListEventArgs e)
+        {
+            m_redoList = new List<string>(e.UndoList);
+        }
+    }
+}
