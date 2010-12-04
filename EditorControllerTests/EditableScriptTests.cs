692ebe99b482d7380e064d9551fce33bd92c842a
Alex Warren <alex@axeuk.com>
2010-12-04 22:46:30 +0000

    Test undo/redo for "msg" script parameter, "if" expression, and "if" "then" script.
    git-svn-id: https://quest.svn.codeplex.com/svn/trunk@3942 4a90e977-6436-4932-9605-20e10c2b6fb3

--- EditorControllerTests/EditableScriptTests.cs
+++ EditorControllerTests/EditableScriptTests.cs
@@ -10,40 +10,67 @@ namespace EditorControllerTests
     [TestClass]
     public class EditableScriptTests : EditorControllerTestBase
     {
-        [TestMethod]
-        public void TestUndoRedo()
+        private void TestUndoRedo(string initialScript, string initialDisplayString, string transactionDescription, Action<IEditableScript> changeValue, string newDisplayString)
         {
             Assert.AreEqual(0, UndoList.Count);
             Assert.AreEqual(0, RedoList.Count);
-            EditableScripts newScripts = Controller.CreateNewEditableScripts("game", "somescript", "msg (\"hello\")");
+            EditableScripts newScripts = Controller.CreateNewEditableScripts("game", "somescript", initialScript);
             Assert.AreEqual(1, newScripts.Scripts.Count());
             Assert.AreEqual(1, UndoList.Count);
             Assert.AreEqual(0, RedoList.Count);
             IEditableScript script = newScripts[0];
-            Assert.AreEqual("Print \"hello\"", script.DisplayString());
-            Controller.StartTransaction("Change msg script value");
-            script.SetParameter(0, "\"new value\"");
+            Assert.AreEqual(initialDisplayString, script.DisplayString());
+            Controller.StartTransaction(transactionDescription);
+            changeValue(script);
             Controller.EndTransaction();
             Assert.AreEqual(2, UndoList.Count);
             Assert.AreEqual(0, RedoList.Count);
-            Assert.AreEqual("Change msg script value", UndoList[0]);
-            Assert.AreEqual("Print \"new value\"", script.DisplayString());
+            Assert.AreEqual(transactionDescription, UndoList[0]);
+            Assert.AreEqual(newDisplayString, script.DisplayString());
             Controller.Undo();
             Assert.AreEqual(1, UndoList.Count);
             Assert.AreEqual(1, RedoList.Count);
-            Assert.AreEqual("Change msg script value", RedoList[0]);
-            Assert.AreEqual("Print \"hello\"", script.DisplayString());
+            Assert.AreEqual(transactionDescription, RedoList[0]);
+            Assert.AreEqual(initialDisplayString, script.DisplayString());
             Controller.Redo();
             Assert.AreEqual(2, UndoList.Count);
             Assert.AreEqual(0, RedoList.Count);
-            Assert.AreEqual("Change msg script value", UndoList[0]);
-            Assert.AreEqual("Print \"new value\"", script.DisplayString());
+            Assert.AreEqual(transactionDescription, UndoList[0]);
+            Assert.AreEqual(newDisplayString, script.DisplayString());
         }
 
         [TestMethod]
-        public void TestIf()
+        public void TestSimpleUndoRedo()
         {
-            EditableScripts newScripts = Controller.CreateNewEditableScripts("game", "somescript", "if () { }");
+            TestUndoRedo(
+                "msg (\"hello\")",
+                "Print \"hello\"",
+                "Change msg script value",
+                (script) => script.SetParameter(0, "\"new value\""),
+                "Print \"new value\"");
         }
+
+        [TestMethod]
+        public void TestIfExpressionUndoRedo()
+        {
+            TestUndoRedo(
+                "if (someExpression) { msg (\"Then script\") }",
+                "If (someExpression) Then 'Print \"Then script\"'",
+                "Change expression",
+                (script) => ((EditableIfScript)script).SetAttribute("expression", "newExpression"),
+                "If (newExpression) Then 'Print \"Then script\"'");
+        }
+
+        [TestMethod]
+        public void TestIfThenUndoRedo()
+        {
+            TestUndoRedo(
+                "if (someExpression) { msg (\"Then script\") }",
+                "If (someExpression) Then 'Print \"Then script\"'",
+                "Change then script",
+                (script) => ((EditableIfScript)script).ThenScript[0].SetParameter(0, "\"new value\""),
+                "If (someExpression) Then 'Print \"new value\"'");
+        }
+
     }
 }
