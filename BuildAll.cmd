d70ed50a2bfe71561113959c46f51d2359d38846
Alex Warren <alex@axeuk.com>
2011-01-08 12:27:15 +0000

    Fixing Setup. Set Target Framework to ".NET 4.0 Client Profile" for all projects.
    git-svn-id: https://quest.svn.codeplex.com/svn/trunk@4891 4a90e977-6436-4932-9605-20e10c2b6fb3

--- BuildAll.cmd
+++ BuildAll.cmd
@@ -1,6 +1,6 @@
-cd C:\VBProjects\Quest\v5.0
+cd C:\VBProjects\Quest (CodePlex)\trunk
 rem C:\Windows\Microsoft.NET\Framework\v3.5\msbuild /property:Configuration=Release .\Quest\Quest.vbproj
-"C:\Program Files\Microsoft Visual Studio 9.0\Common7\IDE\devenv" ".\Quest 5.0.sln" /build "ReleaseSetup"
+"C:\Program Files\Microsoft Visual Studio 10.0\Common7\IDE\devenv" ".\Quest 5.0.sln" /build "ReleaseSetup"
 rd /s /q .\Output\Samples
 svn export .\WorldModel\WorldModel\Examples-Public .\Output\Samples
 "C:\Program Files\7-Zip\7z.exe" a -r .\Output\samples.zip .\Output\Samples\*.*
