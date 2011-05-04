319b85331da8dc71ec372fdfdf79e5c2d0cbbf7a
Alex Warren <alex@axeuk.com>
2011-05-04 19:39:59 +0000

    Implementing Create New Game window
    git-svn-id: https://quest.svn.codeplex.com/svn/trunk@7559 4a90e977-6436-4932-9605-20e10c2b6fb3

--- Editor/NewGameWindow.vb
+++ Editor/NewGameWindow.vb
@@ -3,6 +3,34 @@
     Public Cancelled As Boolean
     Private m_templates As Dictionary(Of String, String)
 
+    Public Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+        SetFilename()
+    End Sub
+
+    Private Sub SetFilename()
+        Dim myDocsFolder As String = System.Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)
+        Dim gamesFolder As String = System.IO.Path.Combine(myDocsFolder, "Quest Games")
+        Dim filename As String = System.IO.Path.Combine(gamesFolder, GenerateFilename(txtGameName.Text))
+        txtFilename.Text = filename
+    End Sub
+
+    Private Shared s_invalidChars As New List(Of Char) From
+        {"\"c, "/"c, ":"c, "*"c, "?"c, """"c, "<"c, ">"c, "|"c}
+
+    Private Function GenerateFilename(gameName As String) As String
+        Dim result = gameName
+        For Each invalidChar In s_invalidChars
+            result = result.Replace(invalidChar, "")
+        Next
+        If result.Length = 0 Then Return String.Empty
+        Return System.IO.Path.Combine(result, result + ".aslx")
+    End Function
+
     Private Sub cmdOK_Click(sender As System.Object, e As System.EventArgs) Handles cmdOK.Click
         Me.Hide()
     End Sub
@@ -24,4 +52,7 @@
         End If
     End Sub
 
+    Private Sub txtGameName_TextChanged(sender As Object, e As System.EventArgs) Handles txtGameName.TextChanged
+        SetFilename()
+    End Sub
 End Class
