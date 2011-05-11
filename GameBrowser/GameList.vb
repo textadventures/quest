2f9ac8136f5f91db259d1fdda7e098226c04007d
Alex Warren <alex@axeuk.com>
2011-05-11 14:00:26 +0000

    Reuse GameListItem controls in GameList so you can still see the latest downloading/downloaded state of a game even when switching between categories.
    git-svn-id: https://quest.svn.codeplex.com/svn/trunk@7726 4a90e977-6436-4932-9605-20e10c2b6fb3

--- GameBrowser/GameList.vb
+++ GameBrowser/GameList.vb
@@ -1,8 +1,7 @@
-﻿Imports Microsoft.Win32
-
-Friend Class GameList
+﻿Friend Class GameList
     Private m_launchCaption As String
-    Private m_gameListItems As New List(Of GameListItem)
+    Private m_visibleGameListItems As New List(Of GameListItem)
+    Private m_gameListItems As New Dictionary(Of String, GameListItem)
 
     Public Event Launch(filename As String)
 
@@ -36,52 +35,61 @@ Friend Class GameList
         For Each data As GameListItemData In list
             count += 1
 
-            newItem = New GameListItem
-            newItem.Dock = DockStyle.Fill
-            newItem.Width = Me.Width
-            newItem.GameName = data.GameName
-            If String.IsNullOrEmpty(data.Filename) Then
-                newItem.URL = data.URL
-
-                Dim downloadFolder As String = System.IO.Path.Combine(
-                    Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),
-                    "Quest Games",
-                    "Downloaded Games")
-
-                System.IO.Directory.CreateDirectory(downloadFolder)
-
-                Dim downloadFilename As String = System.IO.Path.Combine(
-                    downloadFolder, data.DownloadFilename)
-
-                newItem.DownloadFilename = downloadFilename
-
-                If System.IO.File.Exists(downloadFilename) Then
-                    ' The file has already been downloaded
-                    data.Filename = downloadFilename
-                End If
-            End If
-
-            If Not String.IsNullOrEmpty(data.Filename) Then
-                newItem.GameInfo = System.IO.Path.GetFileName(data.Filename)
-                newItem.Filename = data.Filename
-                newItem.LaunchCaption = m_launchCaption
+            If Not String.IsNullOrEmpty(data.DownloadFilename) AndAlso m_gameListItems.ContainsKey(data.DownloadFilename) Then
+                newItem = m_gameListItems(data.DownloadFilename)
             Else
-                newItem.CurrentState = GameListItem.State.NotDownloaded
-            End If
+                newItem = New GameListItem
+                newItem.Dock = DockStyle.Fill
+                newItem.Width = Me.Width
+                newItem.GameName = data.GameName
 
-            AddHandler newItem.Launch, AddressOf LaunchHandler
-            m_gameListItems.Add(newItem)
+                If Not String.IsNullOrEmpty(data.DownloadFilename) Then
+                    m_gameListItems.Add(data.DownloadFilename, newItem)
+                End If
+
+                If String.IsNullOrEmpty(data.Filename) Then
+                    newItem.URL = data.URL
+
+                    Dim downloadFolder As String = System.IO.Path.Combine(
+                        Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),
+                        "Quest Games",
+                        "Downloaded Games")
+
+                    System.IO.Directory.CreateDirectory(downloadFolder)
+
+                    Dim downloadFilename As String = System.IO.Path.Combine(
+                        downloadFolder, data.DownloadFilename)
+
+                    newItem.DownloadFilename = downloadFilename
+
+                    If System.IO.File.Exists(downloadFilename) Then
+                        ' The file has already been downloaded
+                        data.Filename = downloadFilename
+                    End If
+                End If
+
+                If Not String.IsNullOrEmpty(data.Filename) Then
+                    newItem.GameInfo = System.IO.Path.GetFileName(data.Filename)
+                    newItem.Filename = data.Filename
+                    newItem.LaunchCaption = m_launchCaption
+                Else
+                    newItem.CurrentState = GameListItem.State.NotDownloaded
+                End If
+
+                AddHandler newItem.Launch, AddressOf LaunchHandler
+            End If
+            
+            m_visibleGameListItems.Add(newItem)
 
             ctlTableLayout.Controls.Add(newItem)
         Next
     End Sub
 
     Private Sub ClearListElements()
-        For Each item In m_gameListItems
-            RemoveHandler item.Launch, AddressOf LaunchHandler
+        For Each item In m_visibleGameListItems
             ctlTableLayout.Controls.Remove(item)
         Next
-        m_gameListItems.Clear()
+        m_visibleGameListItems.Clear()
     End Sub
 
     Private Sub LaunchHandler(filename As String)
