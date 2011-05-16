6ab47872de023c87059bd40396536949a0d7a2f6
Alex Warren <alex@axeuk.com>
2011-05-16 22:24:03 +0000

    Show a subtle border around the rich text editor
    git-svn-id: https://quest.svn.codeplex.com/svn/trunk@7853 4a90e977-6436-4932-9605-20e10c2b6fb3

--- Editor/RichTextControl.Designer.vb
+++ Editor/RichTextControl.Designer.vb
@@ -23,21 +23,46 @@ Partial Class RichTextControl
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(RichTextControl))
+        Me.pnlContainer = New System.Windows.Forms.Panel()
+        Me.ctlWebBrowser = New System.Windows.Forms.WebBrowser()
         Me.ctlToolbar = New System.Windows.Forms.ToolStrip()
         Me.butBold = New System.Windows.Forms.ToolStripButton()
         Me.butItalic = New System.Windows.Forms.ToolStripButton()
         Me.butUnderline = New System.Windows.Forms.ToolStripButton()
-        Me.ctlWebBrowser = New System.Windows.Forms.WebBrowser()
+        Me.pnlContainer.SuspendLayout()
         Me.ctlToolbar.SuspendLayout()
         Me.SuspendLayout()
+        '
+        'pnlContainer
+        '
+        Me.pnlContainer.BackColor = System.Drawing.SystemColors.Control
+        Me.pnlContainer.Controls.Add(Me.ctlWebBrowser)
+        Me.pnlContainer.Controls.Add(Me.ctlToolbar)
+        Me.pnlContainer.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.pnlContainer.Location = New System.Drawing.Point(0, 0)
+        Me.pnlContainer.Name = "pnlContainer"
+        Me.pnlContainer.Padding = New System.Windows.Forms.Padding(1)
+        Me.pnlContainer.Size = New System.Drawing.Size(469, 263)
+        Me.pnlContainer.TabIndex = 2
+        '
+        'ctlWebBrowser
+        '
+        Me.ctlWebBrowser.AllowWebBrowserDrop = False
+        Me.ctlWebBrowser.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.ctlWebBrowser.IsWebBrowserContextMenuEnabled = False
+        Me.ctlWebBrowser.Location = New System.Drawing.Point(1, 26)
+        Me.ctlWebBrowser.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.ctlWebBrowser.Name = "ctlWebBrowser"
+        Me.ctlWebBrowser.Size = New System.Drawing.Size(467, 236)
+        Me.ctlWebBrowser.TabIndex = 2
         '
         'ctlToolbar
         '
         Me.ctlToolbar.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.butBold, Me.butItalic, Me.butUnderline})
-        Me.ctlToolbar.Location = New System.Drawing.Point(0, 0)
+        Me.ctlToolbar.Location = New System.Drawing.Point(1, 1)
         Me.ctlToolbar.Name = "ctlToolbar"
-        Me.ctlToolbar.Size = New System.Drawing.Size(469, 25)
-        Me.ctlToolbar.TabIndex = 0
+        Me.ctlToolbar.Size = New System.Drawing.Size(467, 25)
+        Me.ctlToolbar.TabIndex = 3
         Me.ctlToolbar.Text = "ToolStrip1"
         '
         'butBold
@@ -67,34 +92,24 @@ Partial Class RichTextControl
         Me.butUnderline.Size = New System.Drawing.Size(23, 22)
         Me.butUnderline.Text = "Underline"
         '
-        'ctlWebBrowser
-        '
-        Me.ctlWebBrowser.AllowWebBrowserDrop = False
-        Me.ctlWebBrowser.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.ctlWebBrowser.IsWebBrowserContextMenuEnabled = False
-        Me.ctlWebBrowser.Location = New System.Drawing.Point(0, 25)
-        Me.ctlWebBrowser.MinimumSize = New System.Drawing.Size(20, 20)
-        Me.ctlWebBrowser.Name = "ctlWebBrowser"
-        Me.ctlWebBrowser.Size = New System.Drawing.Size(469, 238)
-        Me.ctlWebBrowser.TabIndex = 1
-        '
         'RichTextControl
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Controls.Add(Me.ctlWebBrowser)
-        Me.Controls.Add(Me.ctlToolbar)
+        Me.Controls.Add(Me.pnlContainer)
         Me.Name = "RichTextControl"
         Me.Size = New System.Drawing.Size(469, 263)
+        Me.pnlContainer.ResumeLayout(False)
+        Me.pnlContainer.PerformLayout()
         Me.ctlToolbar.ResumeLayout(False)
         Me.ctlToolbar.PerformLayout()
         Me.ResumeLayout(False)
-        Me.PerformLayout()
 
     End Sub
+    Friend WithEvents pnlContainer As System.Windows.Forms.Panel
+    Friend WithEvents ctlWebBrowser As System.Windows.Forms.WebBrowser
     Friend WithEvents ctlToolbar As System.Windows.Forms.ToolStrip
     Friend WithEvents butBold As System.Windows.Forms.ToolStripButton
-    Friend WithEvents ctlWebBrowser As System.Windows.Forms.WebBrowser
     Friend WithEvents butItalic As System.Windows.Forms.ToolStripButton
     Friend WithEvents butUnderline As System.Windows.Forms.ToolStripButton
 
