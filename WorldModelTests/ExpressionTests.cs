fa9b96c30d140f19df3c2372028d3fd68459b03f
Alex Warren <alex@axeuk.com>
2011-05-20 14:04:56 +0000

    Add some expression tests
    git-svn-id: https://quest.svn.codeplex.com/svn/trunk@7953 4a90e977-6436-4932-9605-20e10c2b6fb3

---
+++ WorldModelTests/ExpressionTests.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AxeSoftware.Quest;
+using AxeSoftware.Quest.Functions;
+using AxeSoftware.Quest.Scripts;
+
+namespace WorldModelTests
+{
+    [TestClass]
+    public class ExpressionTests
+    {
+        const string attributeName = "attribute";
+        const string attributeValue = "attributevalue";
+        const string childAttributeValue = "childattributevalue";
+
+        const string intAttributeName = "intattribute";
+        const int intAttributeValue = 42;
+
+        private WorldModel m_worldModel;
+        private Element m_object;
+        private Element m_child;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            m_worldModel = new WorldModel();
+            m_object = m_worldModel.GetElementFactory(ElementType.Object).Create("object");
+            m_object.Fields.Set(attributeName, attributeValue);
+            m_object.Fields.Set(intAttributeName, intAttributeValue);
+
+            m_child = m_worldModel.GetElementFactory(ElementType.Object).Create("child");
+            m_child.Parent = m_object;
+            m_child.Fields.Set(attributeName, childAttributeValue);
+        }
+
+        private T RunExpression<T>(string expression)
+        {
+            Expression<T> expr = new Expression<T>(expression, m_worldModel);
+            Context c = new Context();
+            return expr.Execute(c);
+        }
+
+        [TestMethod]
+        public void TestReadStringFields()
+        {
+            string result;
+            
+            result = RunExpression<string>("object.attribute");
+            Assert.AreEqual(attributeValue, result);
+
+            result = RunExpression<string>("child.attribute");
+            Assert.AreEqual(childAttributeValue, result);
+        }
+
+        [TestMethod]
+        public void TestReadChildParentField()
+        {
+            string result = RunExpression<string>("child.parent.attribute");
+            Assert.AreEqual(attributeValue, result);
+        }
+
+        [TestMethod]
+        public void TestStringConcatenate()
+        {
+            const string extraString = "testconcat";
+            string result = RunExpression<string>("object.attribute + \"" + extraString + "\"");
+            Assert.AreEqual(attributeValue + extraString, result);
+        }
+
+        [TestMethod]
+        public void TestReadIntField()
+        {
+            int result = RunExpression<int>("object.intattribute");
+            Assert.AreEqual(intAttributeValue, result);
+        }
+
+        [TestMethod]
+        public void TestAddition()
+        {
+            int result = RunExpression<int>("object.intattribute + 3");
+            Assert.AreEqual(intAttributeValue + 3, result);
+        }
+    }
+}
