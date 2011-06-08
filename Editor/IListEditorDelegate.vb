873af9f140a03da0824a862cfa1a7e9ebd65b6ea
Alex Warren <alex@axeuk.com>
2011-06-08 12:10:43 +0000

    Merging EditorControls branch (and CodePlex doesn't seem to support SVN Merge - argh!)
    git-svn-id: https://quest.svn.codeplex.com/svn/trunk@8333 4a90e977-6436-4932-9605-20e10c2b6fb3

--- Editor/IListEditorDelegate.vb
+++
@@ -1,5 +0,0 @@
-﻿Public Interface IListEditorDelegate
-    Sub DoAdd()
-    Sub DoEdit(key As String, index As Integer)
-    Sub DoRemove(keys() As String)
-End Interface
--- Editor/Utility.vb
+++
@@ -1,6 +0,0 @@
-﻿Public Class Utility
-    Public Shared Function FormatAsOneLine(input As String) As String
-        If input Is Nothing Then Return Nothing
-        Return input.Replace(Environment.NewLine, " / ")
-    End Function
-End Class
