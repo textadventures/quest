445df188fbb7857f782a43cd35d7f53bfb5c5caa
Alex Warren <alex@axeuk.com>
2011-06-15 10:09:11 +0000

    Tidy up JS and fix scrolling bug
    git-svn-id: https://quest.svn.codeplex.com/svn/trunk@8469 4a90e977-6436-4932-9605-20e10c2b6fb3

--- WorldModel/WorldModel/Core/Frame.js
+++ WorldModel/WorldModel/Core/Frame.js
@@ -1,29 +1,22 @@
 ﻿var textFrame = null;
 
-// using the proxy pattern http://docs.jquery.com/Types#Proxy%5FPattern to override the addText function
-
-(function () {
-    var proxied = window.addText;
-    window.addText = function (text) {
-        if (textFrame == null) {
-            // We can access the original addText function like this. This is required so we can add Frame.htm
-            // to the output window in the first place!
-            return proxied(text);
-        }
-        else {
-            textFrame.append(text);
-            textFrame.animate({ scrollTop: textFrame.height() }, 0);
-        }
-    };
-})();
-
 function beginUsingTextFrame() {
     textFrame = $("#divText");
     setFrameHeight();
     $("body").css("overflow", "hidden");
+    
     $(window).resize(function() {
         setFrameHeight();
     });
+    
+    window.addText = function(text) {
+        textFrame.append(text);
+        scrollToEnd();
+    }
+
+    window.scrollToEnd = function () {
+        textFrame.scrollTop(textFrame.attr("scrollHeight"));
+    }
 }
 
 function setFrameHeight() {
