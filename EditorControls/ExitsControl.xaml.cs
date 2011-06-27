e5ced4849a8a4557f49aec1bae031ae9adaa0a4f
Alex Warren <alex@axeuk.com>
2011-06-27 17:30:35 +0000

    #666 - Compass editor placeholder
    git-svn-id: https://quest.svn.codeplex.com/svn/trunk@8700 4a90e977-6436-4932-9605-20e10c2b6fb3

---
+++ EditorControls/ExitsControl.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AxeSoftware.Quest.EditorControls
+{
+    [ControlType("exits")]
+    public partial class ExitsControl : UserControl, IElementEditorControl, IControlDataHelper
+    {
+        public event EventHandler<DataModifiedEventArgs> Dirty;
+        public event Action RequestParentElementEditorSave;
+        private ControlDataOptions m_options = new ControlDataOptions();
+
+        public ExitsControl()
+        {
+            InitializeComponent();
+        }
+
+        public IControlDataHelper Helper
+        {
+            get { return this; }
+        }
+
+        public void Populate(IEditorData data)
+        {
+        }
+
+        public void Save()
+        {
+        }
+
+        public Control FocusableControl
+        {
+            get { return null; }
+        }
+
+        public Type ExpectedType
+        {
+            get { return null; }
+        }
+
+        public string Attribute
+        {
+            get { return null; }
+        }
+
+        public ControlDataOptions Options
+        {
+            get { return m_options; }
+        }
+
+        public void DoInitialise(EditorController controller, IEditorControl definition)
+        {
+        }
+    }
+}
