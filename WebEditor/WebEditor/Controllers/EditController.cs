7c500427e4e981e1a18bcb740886b0ba3ddb9fd2
Alex Warren <alex@axeuk.com>
2011-10-27 14:54:05 +0100

    WebEditor loads aslx file specified by config and displays title.

---
+++ WebEditor/WebEditor/Controllers/EditController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AxeSoftware.Quest;
+using System.Configuration;
+
+namespace WebEditor.Controllers
+{
+    public class EditController : Controller
+    {
+        //
+        // GET: /Edit/Game/1
+
+        public ActionResult Game(int id)
+        {
+            return View();
+        }
+
+        public ActionResult Load()
+        {
+            EditorController editor = new EditorController();
+            string debugFile = ConfigurationManager.AppSettings["DebugFile"];
+            string libFolder = ConfigurationManager.AppSettings["LibraryFolder"];
+            editor.Initialise(debugFile, libFolder);
+            WebEditor.Models.Game model = new Models.Game();
+            model.Name = editor.GameName;
+            return PartialView("GameInfo", model);
+        }
+    }
+}
---
+++ WebEditor/WebEditor/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebEditor.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            // TO DO: This should be set in config
+            return new RedirectResult("http://www.textadventures.co.uk/");
+        }
+    }
+}
