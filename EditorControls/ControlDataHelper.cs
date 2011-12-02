9898e6b7160e91b555302733292f2bc392327c84
Alex Warren <alex@axeuk.com>
2011-12-02 21:20:34 +0000

    #929 - when adding an "else if", setting expression would cause repopulating, which could mean old control was unpopulated before the transaction finished.

--- EditorControls/ControlDataHelper.cs
+++ EditorControls/ControlDataHelper.cs
@@ -131,21 +131,29 @@ namespace AxeSoftware.Quest.EditorControls
             if (!m_dirty) return;
             if (m_saving) return;
             m_saving = true;
-            if (m_data.IsDirectlySaveable)
+
+            // When we call m_data.SetAttribute below, that can trigger off a whole chain of events which may
+            // cause us to be Unpopulated before we finish the function. We'll still need to finish the transaction,
+            // so we make copies first.
+            bool directlySaveable = m_data.IsDirectlySaveable;
+            EditorController controller = Controller;
+            string caption = ControlDefinition.Caption;
+            
+            if (directlySaveable)
             {
-                Controller.StartTransaction(string.Format("Set {0} to '{1}'", ControlDefinition.Caption, newValue == null ? "null" : newValue.ToString()));
+                controller.StartTransaction(string.Format("Set {0} to '{1}'", ControlDefinition.Caption, newValue == null ? "null" : newValue.ToString()));
             }
             ValidationResult result = m_data.SetAttribute(ControlDefinition.Attribute, newValue);
 
             if (!result.Valid)
             {
                 string errorValue = newValue as string;
-                PopupEditors.DisplayValidationError(result, errorValue, string.Format("Unable to set '{0}'", ControlDefinition.Caption));
+                PopupEditors.DisplayValidationError(result, errorValue, string.Format("Unable to set '{0}'", caption));
             }
 
-            if (m_data.IsDirectlySaveable)
+            if (directlySaveable)
             {
-                Controller.EndTransaction();
+                controller.EndTransaction();
             }
             m_saving = false;
 
