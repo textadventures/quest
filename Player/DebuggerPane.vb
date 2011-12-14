0c18ec486f23e1443a010203a5093f669d6d5ebb
Alex Warren <alex@axeuk.com>
2011-12-14 13:31:38 +0000

    #915 - can now sort debugger lists

--- Player/DebuggerPane.vb
+++ Player/DebuggerPane.vb
@@ -4,6 +4,18 @@ Public Class DebuggerPane
 
     Private WithEvents m_game As IASLDebug
     Private m_filter As String
+    Private m_objectsSorter As New ListViewColumnSorter
+    Private m_attributesSorter As New ListViewColumnSorter
+
+    Public Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+        lstObjects.ListViewItemSorter = m_objectsSorter
+        lstAttributes.ListViewItemSorter = m_attributesSorter
+    End Sub
 
     Public Property Game() As IASLDebug
         Get
@@ -22,8 +34,27 @@ Public Class DebuggerPane
         Next
     End Sub
 
+    Private Sub lstObjects_ColumnClick(sender As Object, e As System.Windows.Forms.ColumnClickEventArgs) Handles lstObjects.ColumnClick
+        SortList(lstObjects, m_objectsSorter, e.Column)
+    End Sub
+
     Private Sub lstObjects_SelectedIndexChanged(sender As System.Object, e As System.EventArgs) Handles lstObjects.SelectedIndexChanged
         UpdateAttributes()
+    End Sub
+
+    Private Sub SortList(listView As ListView, columnSorter As ListViewColumnSorter, column As Integer)
+        If (column = columnSorter.SortColumn) Then
+            If (columnSorter.Order = SortOrder.Ascending) Then
+                columnSorter.Order = SortOrder.Descending
+            Else
+                columnSorter.Order = SortOrder.Ascending
+            End If
+        Else
+            columnSorter.SortColumn = column
+            columnSorter.Order = SortOrder.Ascending
+        End If
+
+        listView.Sort()
     End Sub
 
     Private Sub UpdateAttributes()
@@ -55,4 +86,75 @@ Public Class DebuggerPane
         End Set
     End Property
 
+    Private Sub lstAttributes_ColumnClick(sender As Object, e As System.Windows.Forms.ColumnClickEventArgs) Handles lstAttributes.ColumnClick
+        SortList(lstAttributes, m_attributesSorter, e.Column)
+    End Sub
+End Class
+
+' From http://msdn.microsoft.com/en-us/kb/kbarticle.aspx?id=319399
+Public Class ListViewColumnSorter
+    Implements System.Collections.IComparer
+
+    Private ColumnToSort As Integer
+    Private OrderOfSort As SortOrder
+    Private ObjectCompare As CaseInsensitiveComparer
+
+    Public Sub New()
+        ' Initialize the column to '0'.
+        ColumnToSort = 0
+
+        ' Initialize the sort order to 'none'.
+        OrderOfSort = SortOrder.None
+
+        ' Initialize the CaseInsensitiveComparer object.
+        ObjectCompare = New CaseInsensitiveComparer()
+    End Sub
+
+    Public Function Compare(ByVal x As Object, ByVal y As Object) As Integer Implements IComparer.Compare
+        Dim compareResult As Integer
+        Dim listviewX As ListViewItem
+        Dim listviewY As ListViewItem
+
+        ' Cast the objects to be compared to ListViewItem objects.
+        listviewX = CType(x, ListViewItem)
+        listviewY = CType(y, ListViewItem)
+
+        ' Compare the two items.
+        compareResult = ObjectCompare.Compare(listviewX.SubItems(ColumnToSort).Text, listviewY.SubItems(ColumnToSort).Text)
+
+        ' Calculate the correct return value based on the object 
+        ' comparison.
+        If (OrderOfSort = SortOrder.Ascending) Then
+            ' Ascending sort is selected, return typical result of 
+            ' compare operation.
+            Return compareResult
+        ElseIf (OrderOfSort = SortOrder.Descending) Then
+            ' Descending sort is selected, return negative result of 
+            ' compare operation.
+            Return (-compareResult)
+        Else
+            ' Return '0' to indicate that they are equal.
+            Return 0
+        End If
+    End Function
+
+    Public Property SortColumn() As Integer
+        Set(ByVal Value As Integer)
+            ColumnToSort = Value
+        End Set
+
+        Get
+            Return ColumnToSort
+        End Get
+    End Property
+
+    Public Property Order() As SortOrder
+        Set(ByVal Value As SortOrder)
+            OrderOfSort = Value
+        End Set
+
+        Get
+            Return OrderOfSort
+        End Get
+    End Property
 End Class
