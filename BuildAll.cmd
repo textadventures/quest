d0c5d5d6563dd1c6a49bde00d68e9972bfc01127
Alex Warren <alex@axeuk.com>
2012-03-19 23:09:30 +0000

    Delete old file

--- BuildAll.cmd
+++
@@ -1,8 +0,0 @@
-cd C:\VBProjects\Quest (CodePlex)\trunk
-rem C:\Windows\Microsoft.NET\Framework\v3.5\msbuild /property:Configuration=Release .\Quest\Quest.vbproj
-"C:\Program Files\Microsoft Visual Studio 10.0\Common7\IDE\devenv" ".\Quest 5.0.sln" /build "ReleaseSetup"
-rd /s /q .\Output\Samples
-svn export .\WorldModel\WorldModel\Examples-Public .\Output\Samples
-"C:\Program Files\7-Zip\7z.exe" a -r .\Output\samples.zip .\Output\Samples\*.*
-"C:\Program Files\7-Zip\7z.exe" a -r .\Output\quest500preview.zip .\Setup\ReleaseSetup\*.*
-pause
