d8b371d6eb637e6b19d24c9b159d92ac7edfe1bb
Alex Warren <alex@axeuk.com>
2012-10-05 17:27:27 +0100

    Remove some old example files

--- WorldModel/WorldModel/Examples-Public/demo/YouTube.js
+++
@@ -1,4 +0,0 @@
-function AddYouTube(id) {
-    var embedHTML = "<object width=\"425\" height=\"344\"><param name=\"movie\" value=\"http://www.youtube.com/v/" + id + "\"></param><param name=\"allowFullScreen\" value=\"true\"></param><param name=\"allowscriptaccess\" value=\"always\"></param><embed src=\"http://www.youtube.com/v/" + id + "\" type=\"application/x-shockwave-flash\" allowscriptaccess=\"always\" allowfullscreen=\"true\" width=\"425\" height=\"344\"></embed></object>";
-    addText(embedHTML);
-}
