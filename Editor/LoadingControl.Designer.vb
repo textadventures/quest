4f15f185a0db15eff53bcc21e384d71ab658a111
Alex Warren <alex@axeuk.com>
2013-01-02 14:37:30 +0000

    Logo was slightly clipped on editor loading screen

--- Editor/LoadingControl.Designer.vb
+++ Editor/LoadingControl.Designer.vb
@@ -72,7 +72,7 @@ Partial Class LoadingControl
         Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
         Me.PictureBox1.Location = New System.Drawing.Point(228, 75)
         Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(134, 133)
+        Me.PictureBox1.Size = New System.Drawing.Size(134, 141)
         Me.PictureBox1.TabIndex = 9
         Me.PictureBox1.TabStop = False
         '
