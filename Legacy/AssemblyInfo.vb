5978666971408e41767423a8db3ae9a00576a84b
Alex Warren <alex@axeuk.com>
2013-01-03 17:23:52 +0000

    Default branch now trunk for Quest 5.4, stable is Quest 5.3

--- Legacy/AssemblyInfo.vb
+++ Legacy/AssemblyInfo.vb
@@ -28,7 +28,7 @@ Imports System.Runtime.InteropServices
 ' You can specify all the values or you can default the Build and Revision Numbers
 ' by using the '*' as shown below:
 
-<Assembly: AssemblyVersion("5.3.*")> 
+<Assembly: AssemblyVersion("5.4.*")> 
 
 
 
