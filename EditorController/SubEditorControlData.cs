7c7a33d8962ccfc8076bc2d8f16e32a023db0d2b
Alex Warren <alex@axeuk.com>
2013-02-04 10:09:33 +0000

    Support for double type had gone missing from attributes editor

--- EditorController/SubEditorControlData.cs
+++ EditorController/SubEditorControlData.cs
@@ -8,130 +8,128 @@ namespace TextAdventures.Quest
     public class AttributeSubEditorControlData:IEditorControl
     {
         private static Dictionary<string, string> s_allTypes = new Dictionary<string, string> {
-		        {"string","String"},
-		        {"boolean","Boolean"},
-		        {"int","Integer"},
-		        {"script","Script"},
-		        {"stringlist","String List"},
-		        {"object","Object"},
-		        {"simplepattern","Command pattern"},
-                {"stringdictionary","String dictionary"},
-		        {"scriptdictionary","Script dictionary"},
-		        {"null","Null"}
-	        };
+            {"string","String"},
+            {"boolean","Boolean"},
+            {"int","Integer"},
+            {"double","Double"},
+            {"script","Script"},
+            {"stringlist","String List"},
+            {"object","Object"},
+            {"simplepattern","Command pattern"},
+            {"stringdictionary","String dictionary"},
+            {"scriptdictionary","Script dictionary"},
+            {"null","Null"}
+        };
 
-            private string m_attribute;
+        private string m_attribute;
 
-            public AttributeSubEditorControlData(string attribute)
+        public AttributeSubEditorControlData(string attribute)
+        {
+            m_attribute = attribute;
+        }
+
+        protected virtual Dictionary<string, string> AllowedTypes
+        {
+            get { return s_allTypes; }
+        }
+
+        public string Attribute
+        {
+            get { return m_attribute; }
+        }
+
+        public string Caption
+        {
+            get { return null; }
+        }
+
+        public string ControlType
+        {
+            get { return null; }
+        }
+
+        public bool Expand
+        {
+            get { return false; }
+        }
+
+        public bool GetBool(string tag)
+        {
+            return false;
+        }
+
+        public IDictionary<string, string> GetDictionary(string tag)
+        {
+            if (tag == "types")
             {
-                m_attribute = attribute;
+                return AllowedTypes;
             }
-
-            protected virtual Dictionary<string, string> AllowedTypes
+            if (tag == "editors")
             {
-                get { return s_allTypes; }
+                return null;
             }
+            throw new NotImplementedException();
+        }
 
-            public string Attribute
-            {
-                get { return m_attribute; }
-            }
+        public int? GetInt(string tag)
+        {
+            return null;
+        }
 
-            public string Caption
-            {
-                get { return null; }
-            }
+        public double? GetDouble(string tag)
+        {
+            return null;
+        }
 
-            public string ControlType
-            {
-                get { return null; }
-            }
+        public IEnumerable<string> GetListString(string tag)
+        {
+            throw new NotImplementedException();
+        }
 
-            public bool Expand
+        public virtual string GetString(string tag)
+        {
+            switch (tag)
             {
-                get { return false; }
-            }
-
-            public bool GetBool(string tag)
-            {
-                return false;
-            }
-
-            public System.Collections.Generic.IDictionary<string, string> GetDictionary(string tag)
-            {
-                if (tag == "types")
-                {
-                    return AllowedTypes;
-                }
-                else if (tag == "editors")
-                {
+                case "checkbox":
+                    return "True";
+                case "editprompt":
+                case "valueprompt":
+                    return "Please enter a value";
+                case "keyprompt":
+                    return "Please enter a key";
+                default:
                     return null;
-                }
-                else
-                {
-                    throw new NotImplementedException();
-                }
             }
+        }
 
-            public int? GetInt(string tag)
-            {
-                return null;
-            }
+        public int? Height
+        {
+            get { return null; }
+        }
 
-            public double? GetDouble(string tag)
-            {
-                return null;
-            }
+        public bool IsControlVisible(IEditorData data)
+        {
+            return true;
+        }
 
-            public System.Collections.Generic.IEnumerable<string> GetListString(string tag)
-            {
-                throw new NotImplementedException();
-            }
+        public int? Width
+        {
+            get { return null; }
+        }
 
-            public virtual string GetString(string tag)
-            {
-                switch (tag)
-                {
-                    case "checkbox":
-                        return "True";
-                    case "editprompt":
-                    case "valueprompt":
-                        return "Please enter a value";
-                    case "keyprompt":
-                        return "Please enter a key";
-                    default:
-                        return null;
-                }
-            }
+        public IEditorDefinition Parent
+        {
+            get { return null; }
+        }
 
-            public int? Height
-            {
-                get { return null; }
-            }
+        public bool IsControlVisibleInSimpleMode
+        {
+            get { return true; }
+        }
 
-            public bool IsControlVisible(IEditorData data)
-            {
-                return true;
-            }
-
-            public int? Width
-            {
-                get { return null; }
-            }
-
-            public IEditorDefinition Parent
-            {
-                get { return null; }
-            }
-
-            public bool IsControlVisibleInSimpleMode
-            {
-                get { return true; }
-            }
-
-            public string Id
-            {
-                get { throw new NotImplementedException(); }
-            }
+        public string Id
+        {
+            get { throw new NotImplementedException(); }
+        }
     }
 }
