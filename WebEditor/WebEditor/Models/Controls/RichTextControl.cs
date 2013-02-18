0a0e8b00981daee8258e9238b157793568c9f1a8
Alex Warren <alex@axeuk.com>
2013-02-18 17:16:36 +0000

    Refactor richtext control into its own partial view

---
+++ WebEditor/WebEditor/Models/Controls/RichTextControl.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TextAdventures.Quest;
+
+namespace WebEditor.Models.Controls
+{
+    public class RichTextControl
+    {
+        public IEditorControl Control { get; set; }
+        public string Value { get; set; }
+    }
+}
