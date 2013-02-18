77bceb482aa5cc1cc79f842f1a379fb157283709
Alex Warren <alex@axeuk.com>
2013-02-18 17:08:42 +0000

    A little refactoring of the element editor

---
+++ WebEditor/WebEditor/Models/Controls/EditorControl.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TextAdventures.Quest;
+
+namespace WebEditor.Models.Controls
+{
+    public class EditorControl
+    {
+        public int GameId { get; set; }
+        public string Key { get; set; }
+        public IEditorData EditorData { get; set; }
+        public IEditorControl Control { get; set; }
+        public EditorController Controller { get; set; }
+        public bool IsFirst { get; set; }
+        public string ControlType { get; set; }
+        public string Caption { get; set; }
+    }
+}
---
+++ WebEditor/WebEditor/Models/Controls/MultiControl.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TextAdventures.Quest;
+
+namespace WebEditor.Models.Controls
+{
+    public class MultiControl
+    {
+        public int GameId { get; set; }
+        public string Key { get; set; }
+        public IEditorData EditorData { get; set; }
+        public IEditorControl Control { get; set; }
+        public EditorController Controller { get; set; }
+        public object Value { get; set; }
+    }
+}
