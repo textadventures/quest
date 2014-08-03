aaee60cae2c6ac7764167c3f2453c2c68b760650
Alex Warren <alex@textadventures.co.uk>
2014-08-03 15:49:12 +0100

    Version 5.5.1

--- Quest/Constants.vb
+++ Quest/Constants.vb
@@ -1,6 +1,6 @@
 ﻿Public Class Constants
 
-    Public Const QuestVersion As String = "5.5"
+    Public Const QuestVersion As String = "5.5.1"
     Public Const MaxASLVersion As Integer = 550
 
 End Class
