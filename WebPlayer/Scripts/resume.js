984cb3741e82dacea7d0eb0327508f4453ac4735
Alex Warren <alex@textadventures.co.uk>
2014-12-29 15:52:10 +0000

    Download user's save game data

---
+++ WebPlayer/Scripts/resume.js
@@ -0,0 +1,24 @@
+﻿var $_GET = {};
+
+document.location.search.replace(/\??(?:([^=]+)=([^&]*)&?)/g, function () {
+    function decode(s) {
+        return decodeURIComponent(s.split("+").join(" "));
+    }
+
+    $_GET[decode(arguments[1])] = decode(arguments[2]);
+});
+
+$(function () {
+    var id = $_GET["id"];
+    console.log("getting...  " + "http://textadventures.co.uk/games/load/" + id);
+    $.ajax({
+        url: "http://textadventures.co.uk/games/load/" + id,
+        success: function(result) {
+            console.log(result);
+            console.log("done");
+        },
+        xhrFields: {
+            withCredentials: true
+        }
+    });
+});
