22535dddc95d45d31bb4ba3274cdf196d6101bd8
Alex Warren <alex@textadventures.co.uk>
2015-01-04 19:39:27 +0000

    Add FilterConfig

--- WebPlayer/Global.asax.cs
+++ WebPlayer/Global.asax.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 using System.Web.Routing;
 using System.Web.Security;
 using System.Web.SessionState;
@@ -14,6 +15,7 @@ namespace WebPlayer
         {
             Logging.Log.Info("Application Start");
 
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
 
