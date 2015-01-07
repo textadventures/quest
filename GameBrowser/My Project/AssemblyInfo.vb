b96ed05446ac7c7e882012f113d747b67ea49bfd
Alex Warren <alex@textadventures.co.uk>
2015-01-07 19:34:21 +0000

    Update version to 5.6

--- GameBrowser/My Project/AssemblyInfo.vb
+++ GameBrowser/My Project/AssemblyInfo.vb
@@ -30,4 +30,4 @@ Imports System.Runtime.InteropServices
 ' You can specify all the values or you can default the Build and Revision Numbers 
 ' by using the '*' as shown below:
 
-<Assembly: AssemblyVersion("5.5.*")> 
+<Assembly: AssemblyVersion("5.6.*")> 
