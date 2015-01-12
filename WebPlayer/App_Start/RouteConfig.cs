a51b0b377ab08537b8b61eede64c322e94aabbb0
Alex Warren <alex@textadventures.co.uk>
2015-01-12 19:33:59 +0000

    Redirect HTML games from /games

--- WebPlayer/App_Start/RouteConfig.cs
+++ WebPlayer/App_Start/RouteConfig.cs
@@ -10,6 +10,12 @@ namespace WebPlayer
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "RedirectGames",
+                url: "games/{id}/{*path}",
+                defaults: new { controller = "Redirect", action = "Games" }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
