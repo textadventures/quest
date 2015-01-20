077872e99d3d0744a2dcf00bd6df861b0587d42a
Alex Warren <alex@textadventures.co.uk>
2015-01-20 21:10:46 +0000

    Don't use output logger if not... using it

--- WebPlayer/OutputBuffer.cs
+++ WebPlayer/OutputBuffer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Web;
@@ -13,10 +14,12 @@ namespace WebPlayer
         private readonly List<string> m_javaScriptBuffer = new List<string>();
         private readonly StringBuilder m_outputLoggerText = new StringBuilder();
         private readonly AzureOutputLogger m_outputLogger = new AzureOutputLogger();
+        private readonly bool m_useOutputLogger;
 
         public OutputBuffer(string gameId)
         {
             m_gameId = gameId;
+            m_useOutputLogger = !string.IsNullOrEmpty(ConfigurationManager.AppSettings["AzureLogSessionsBlob"]);
         }
 
         public void AddJavaScriptToBuffer(string function, params IJavaScriptParameter[] parameters)
@@ -25,7 +28,7 @@ namespace WebPlayer
             {
                 m_javaScriptBuffer.Add(string.Format("{0}({1})", function, string.Join(",", parameters.Select(p => p.GetParameter()))));
 
-                if (function.StartsWith("addText"))
+                if (m_useOutputLogger && function.StartsWith("addText"))
                 {
                     string text = parameters.First().GetParameter();
                     text = text.Substring(1, text.Length - 2);
@@ -43,11 +46,14 @@ namespace WebPlayer
                 output = new List<string>(m_javaScriptBuffer);
                 m_javaScriptBuffer.Clear();
 
-                string text = m_outputLoggerText.ToString();
-                if (text.Length > 0)
+                if (m_useOutputLogger)
                 {
-                    m_outputLoggerText.Clear();
-                    m_outputLogger.AddText(text);
+                    string text = m_outputLoggerText.ToString();
+                    if (text.Length > 0)
+                    {
+                        m_outputLoggerText.Clear();
+                        m_outputLogger.AddText(text);
+                    }
                 }
             }
             return output;
