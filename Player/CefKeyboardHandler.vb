1453ab6c44142ff2c15741214f0d26e00698520c
Alex Warren <alex@textadventures.co.uk>
2015-01-27 22:19:56 +0000

    Update CefSharp

--- Player/CefKeyboardHandler.vb
+++ Player/CefKeyboardHandler.vb
@@ -5,7 +5,7 @@ Public Class CefKeyboardHandler
 
     Public Event KeyPressed(code As Keys)
 
-    Public Function OnKeyEvent(browser As IWebBrowser, type As KeyType, code As Integer, modifiers As Integer, isSystemKey As Boolean) As Boolean Implements IKeyboardHandler.OnKeyEvent
+    Public Function OnKeyEvent(browser As IWebBrowser, type As KeyType, code As Integer, modifiers As CefEventFlags, isSystemKey As Boolean) As Boolean Implements IKeyboardHandler.OnKeyEvent
         If type = KeyType.KeyUp Then
             Dim result As Keys = CType(code, Keys)
             If (modifiers And 1) = 1 Then result = result Or Keys.Shift
@@ -15,7 +15,7 @@ Public Class CefKeyboardHandler
         End If
     End Function
 
-    Public Function OnPreKeyEvent(browser As IWebBrowser, type As KeyType, windowsKeyCode As Integer, nativeKeyCode As Integer, modifiers As Integer, isSystemKey As Boolean, isKeyboardShortcut As Boolean) As Boolean Implements IKeyboardHandler.OnPreKeyEvent
+    Public Function OnPreKeyEvent(browser As IWebBrowser, type As KeyType, windowsKeyCode As Integer, nativeKeyCode As Integer, modifiers As CefEventFlags, isSystemKey As Boolean, isKeyboardShortcut As Boolean) As Boolean Implements IKeyboardHandler.OnPreKeyEvent
 
     End Function
 End Class
