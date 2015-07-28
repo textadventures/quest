2b275936c30adac72b2f4a95bc01f4304276dbd6
Alex Warren <alex@textadventures.co.uk>
2015-07-28 08:52:51 +0100

    UTF8 when loading game from Azure

--- WorldModel/WorldModel/GameLoader/GameLoader.cs
+++ WorldModel/WorldModel/GameLoader/GameLoader.cs
@@ -91,6 +91,7 @@ namespace TextAdventures.Quest
                     {
                         using (var client = new WebClient())
                         {
+                            client.Encoding = System.Text.Encoding.UTF8;
                             data = client.DownloadString(filename);
                         }
                     }
