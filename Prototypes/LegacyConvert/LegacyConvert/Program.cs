9fe394529ee6e0a1abad6f604c6a8240ea84ac21
Alex Warren <alex@textadventures.co.uk>
2015-08-08 18:42:36 +0100

    Convert enums

--- Prototypes/LegacyConvert/LegacyConvert/Program.cs
+++ Prototypes/LegacyConvert/LegacyConvert/Program.cs
@@ -17,9 +17,11 @@ namespace LegacyConvert
             var tree = VisualBasicSyntaxTree.ParseText(text);
             var root = (CompilationUnitSyntax)tree.GetRoot();
 
-            var sb = new StringBuilder();
+            var prepend = new StringBuilder();
+            var append = new StringBuilder();
 
-            var result = ProcessNode(root, -1, sb) + sb.ToString();
+            var result = ProcessNode(root, -1, append, prepend);
+            result = prepend.ToString() + result + append.ToString();
 
             System.IO.File.WriteAllText("output.ts", result);
 
@@ -27,25 +29,34 @@ namespace LegacyConvert
             Console.ReadKey();
         }
 
-        static string ProcessNode(SyntaxNode node, int depth, StringBuilder append)
+        static string ProcessNode(SyntaxNode node, int depth, StringBuilder append, StringBuilder prepend)
         {
             switch (node.Kind())
             {
                 case SyntaxKind.CompilationUnit:
-                    return ProcessChildNodes(node, depth, append);
+                    return ProcessChildNodes(node, depth, append, prepend);
                 case SyntaxKind.OptionStatement:
                 case SyntaxKind.ImportsStatement:
                 case SyntaxKind.ClassStatement:
                 case SyntaxKind.EndClassStatement:
                 case SyntaxKind.ImplementsStatement:
+                case SyntaxKind.EnumStatement:
+                case SyntaxKind.EndEnumStatement:
                     // ignore;
                     break;
                 case SyntaxKind.ClassBlock:
-                    var name = ((ClassStatementSyntax)node.ChildNodes().First()).Identifier.Text;
-                    var classResult = string.Format("class {0} {{\n{1}}}\n", name, ProcessChildNodes(node, depth, append));
+                    var className = ((ClassStatementSyntax)node.ChildNodes().First()).Identifier.Text;
+                    var classResult = string.Format("class {0} {{\n{1}}}\n", className, ProcessChildNodes(node, depth, append, prepend));
                     if (depth == 0) return classResult;
                     append.Append(classResult);
                     return null;
+                case SyntaxKind.EnumBlock:
+                    var enumName = ((EnumStatementSyntax)node.ChildNodes().First()).Identifier.Text;
+                    var values = node.ChildNodes().OfType<EnumMemberDeclarationSyntax>().Select(n => n.Identifier.Text);
+                    prepend.Append(string.Format("enum {0} {{{1}}};\n", enumName, string.Join(", ", values)));
+                    return null;
+                case SyntaxKind.EnumMemberDeclaration:
+                    return ((EnumMemberDeclarationSyntax)node).Identifier.Text;
                 default:
                     return string.Format("{0}// UNKNOWN {1}\n", Tabs(depth), node.Kind());
             }
@@ -53,13 +64,12 @@ namespace LegacyConvert
             return null;
         }
 
-        static string ProcessChildNodes(SyntaxNode node, int depth, StringBuilder append)
+        static string ProcessChildNodes(SyntaxNode node, int depth, StringBuilder append, StringBuilder prepend)
         {
             var sb = new StringBuilder();
-            var count = 0;
             foreach (var childNode in node.ChildNodes())
             {
-                sb.Append(ProcessNode(childNode, depth + 1, append));
+                sb.Append(ProcessNode(childNode, depth + 1, append, prepend));
             }
             return sb.ToString();
         }
