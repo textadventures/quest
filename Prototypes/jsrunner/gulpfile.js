d2241cf3047565049d5216efaa040f034e3fdf89
Alex Warren <alex@textadventures.co.uk>
2015-09-06 13:02:22 +0100

    Now using ES6->Babel with Gulp

---
+++ Prototypes/jsrunner/gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp');  
+var sourcemaps = require('gulp-sourcemaps');  
+var ts = require('gulp-typescript');  
+var babel = require('gulp-babel');
+
+var tsProject = ts.createProject('./tsconfig.json');
+
+gulp.task('default', function() {
+    gulp.src('./node_modules/gulp-babel/node_modules/babel-core/browser-polyfill.min.js')
+        .pipe(gulp.dest('.'));
+      
+    return gulp.src('asl4.ts')
+        .pipe(sourcemaps.init())
+        .pipe(ts(tsProject))
+        .pipe(babel())
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('.'));
+});
