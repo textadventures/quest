f54c8ef2814c985862dbff79b44a251cd21b831d
Alex Warren <alex@textadventures.co.uk>
2015-11-17 19:09:40 +0000

    Use latest TypeScript

--- Prototypes/jsrunner/gulpfile.js
+++ Prototypes/jsrunner/gulpfile.js
@@ -3,14 +3,7 @@ var sourcemaps = require('gulp-sourcemaps');
 var ts = require('gulp-typescript');  
 var babel = require('gulp-babel');
 var uglify = require('gulp-uglify');
-
-// when gulp-typescript works with TS1.6, this can be replaced with:
-//    var tsProject = ts.createProject('./tsconfig.json');
-// and the dev dependency on Microsoft/TypeScript can be removed from package.json
-
-var tsProject = ts.createProject('./tsconfig.json', {
-    typescript: require('typescript')
-});
+var tsProject = ts.createProject('./tsconfig.json');
 
 gulp.task('default', function() {
     gulp.src('./node_modules/gulp-babel/node_modules/babel-core/browser-polyfill.min.js')
