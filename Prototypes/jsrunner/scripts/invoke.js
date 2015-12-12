fb7bd3914fd93c068429b5606fe9995128730e93
Alex Warren <alex@textadventures.co.uk>
2015-12-12 11:12:16 +0000

    Invoke can take two parameters

--- Prototypes/jsrunner/scripts/invoke.js
+++ Prototypes/jsrunner/scripts/invoke.js
@@ -1,7 +1,8 @@
 define(['scriptrunner'], function (scriptrunner) {
     return {
-        parameters: [1],
+        parameters: [1, 2],
         execute: function (ctx) {
+            // TODO: Second parameter is a dictionary of parameters to pass in as locals
             scriptrunner.evaluateExpression(ctx.parameters[0], function (result) {
                 scriptrunner.getCallstack().push({
                     script: result,
