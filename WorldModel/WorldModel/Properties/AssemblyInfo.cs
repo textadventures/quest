002fdfd32d6bd13483e36881a9994813afb98b01
ThePix <the_pix@hotmail.com>
2017-03-13 12:54:33 +0000

    Updated copyright

--- WorldModel/WorldModel/Properties/AssemblyInfo.cs
+++ WorldModel/WorldModel/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Alex Warren")]
 [assembly: AssemblyProduct("Quest")]
-[assembly: AssemblyCopyright("Copyright © 2016 Alex Warren")]
+[assembly: AssemblyCopyright("Copyright © 2017 Andy Joel - full copyright and license information at https://github.com/textadventures/quest/blob/master/LICENSE")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("5.6.*")]
+[assembly: AssemblyVersion("5.7.*")]
 
 [assembly: InternalsVisibleTo("WorldModelTests")]
