cf44b624ce56aca9ea678a7e434cf7ebb62f0e7b
ThePix <the_pix@hotmail.com>
2018-08-08 10:14:06 +0100

    Further correctin issues with requestsave

---
+++ WorldModel/WorldModel/Scripts/RequestSaveScript.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TextAdventures.Quest.Functions;
+
+/*
+ * This script command is an alternative to request (Save, ""), and is added as part of deprecating
+ * request. It is called requestsave to replace RequestSave (an earlier step to deprecation), and
+ * to hopefully avoid name clashes in existing games.
+ */
+
+namespace TextAdventures.Quest.Scripts
+{
+    public class RequestSaveScriptConstructor : ScriptConstructorBase
+    {
+        public override string Keyword
+        {
+            get { return "requestsave"; }
+        }
+
+        protected override IScript CreateInt(List<string> parameters, ScriptContext scriptContext)
+        {
+            return new RequestSaveScript(WorldModel);
+        }
+
+        protected override int[] ExpectedParameters
+        {
+            get { return new int[] { 0 }; }
+        }
+    }
+
+    public class RequestSaveScript : ScriptBase
+    {
+        private WorldModel m_worldModel;
+
+        public RequestSaveScript(WorldModel worldModel)
+        {
+            m_worldModel = worldModel;
+        }
+
+        protected override ScriptBase CloneScript()
+        {
+            return new RequestSaveScript(m_worldModel);
+        }
+
+        public override void Execute(Context c)
+        {
+            m_worldModel.PlayerUI.RequestSave(null);
+        }
+
+        public override string Save()
+        {
+            return "requestsave";
+        }
+
+        public override string Keyword
+        {
+            get
+            {
+                return "requestsave";
+            }
+        }
+
+        public override object GetParameter(int index)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+
+        public override void SetParameterInternal(int index, object value)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+    }
+}
