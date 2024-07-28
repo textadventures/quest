6fa81f0e9d6341b653708a32e78011f09bd44195
Alex Warren <alex@alexwarren.uk>
2024-07-29 00:18:20 +0100

    Update QuestConsole to .NET 8

--- QuestConsole/Properties/AssemblyInfo.cs
+++ QuestConsole/Properties/AssemblyInfo.cs
@@ -1,16 +1,6 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("QuestConsole")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Alex Warren")]
-[assembly: AssemblyProduct("Quest")]
-[assembly: AssemblyCopyright("Copyright © 2015 Alex Warren")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -21,15 +11,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("4bf123c2-d5e0-4fd6-bd43-bc95c362cdb3")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("5.6.*")]
