f95e4487dae6875b024202e4aeab9e54f8efb918
Alex Warren <alex@alexwarren.uk>
2024-07-29 00:16:39 +0100

    Restore QuestConsole

---
+++ QuestConsole/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace QuestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Usage: QUESTCONSOLE.EXE <filename>");
+                return;
+            }
+
+            string filename = args[0];
+            Runner runner = new Runner(filename);
+            runner.Start();
+        }
+    }
+}
