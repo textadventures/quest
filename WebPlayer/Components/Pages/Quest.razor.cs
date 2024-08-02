1f4585b344b67649d929c47dcc043e1189e77279
Alex Warren <alex@alexwarren.uk>
2024-08-02 21:07:23 +0100

    Show interface elements

--- WebPlayer/Components/Pages/Quest.razor.cs
+++ WebPlayer/Components/Pages/Quest.razor.cs
@@ -9,6 +9,8 @@ public partial class Quest : ComponentBase, IPlayerHelperUI
     [Parameter] public required string Id { get; set; }
     [Inject] private IJSRuntime JS { get; set; } = null!;
 
+    private PlayerHelper PlayerHelper = null!; 
+
     private string input = string.Empty;
 
     private MarkupString uiHtml = (MarkupString) PlayerHelper.GetResource("playercore.htm");
@@ -50,16 +52,16 @@ public partial class Quest : ComponentBase, IPlayerHelperUI
         // check if they are needed and if there's a better way of getting this.
         
         var game = GameLauncher.GetGame(filename, null);
-        var playerHelper = new PlayerHelper(game, this);
+        PlayerHelper = new PlayerHelper(game, this);
         
         // TODO: Add playerHelper event handlers
         
-        var result = playerHelper.Initialise(this, out var errors);
+        var result = PlayerHelper.Initialise(this, out var errors);
 
         if (result)
         {
-            playerHelper.Game.Begin();
-            await OutputTextNow(playerHelper.ClearBuffer());
+            PlayerHelper.Game.Begin();
+            await OutputTextNow(PlayerHelper.ClearBuffer());
         }
         else
         {
@@ -82,30 +84,39 @@ public partial class Quest : ComponentBase, IPlayerHelperUI
 
     void IPlayerHelperUI.SetAlignment(string alignment)
     {
+        if (alignment.Length == 0) alignment = "left";
+        OutputText(PlayerHelper.ClearBuffer());
+        AddJavaScriptToBuffer("createNewDiv", alignment);
     }
 
     void IPlayerHelperUI.BindMenu(string linkid, string verbs, string text, string elementId)
     {
+        AddJavaScriptToBuffer("bindMenu", linkid, verbs, text, elementId);
     }
 
     void IPlayer.ShowMenu(MenuData menuData)
     {
+        // TODO
     }
 
     void IPlayer.DoWait()
     {
+        // TODO
     }
 
     void IPlayer.DoPause(int ms)
     {
+        // TODO
     }
 
     void IPlayer.ShowQuestion(string caption)
     {
+        // TODO
     }
 
     void IPlayer.SetWindowMenu(MenuData menuData)
     {
+        // TODO
     }
 
     string IPlayer.GetNewGameFile(string originalFilename, string extensions)
@@ -115,14 +126,17 @@ public partial class Quest : ComponentBase, IPlayerHelperUI
 
     void IPlayer.PlaySound(string filename, bool synchronous, bool looped)
     {
+        // TODO
     }
 
     void IPlayer.StopSound()
     {
+        // TODO
     }
 
     void IPlayer.WriteHTML(string html)
     {
+        OutputText(html);
     }
 
     string IPlayer.GetURL(string file)
@@ -132,7 +146,9 @@ public partial class Quest : ComponentBase, IPlayerHelperUI
 
     void IPlayer.LocationUpdated(string location)
     {
+        AddJavaScriptToBuffer("updateLocation", location);
     }
+    
 
     void IPlayer.UpdateGameName(string name)
     {
@@ -141,78 +157,118 @@ public partial class Quest : ComponentBase, IPlayerHelperUI
 
     void IPlayer.ClearScreen()
     {
+        OutputText(PlayerHelper.ClearBuffer());
+        AddJavaScriptToBuffer("clearScreen");
     }
 
     void IPlayer.ShowPicture(string filename)
     {
+        // TODO
     }
 
     void IPlayer.SetPanesVisible(string data)
     {
+        AddJavaScriptToBuffer("panesVisible", data == "on");
     }
 
     void IPlayer.SetStatusText(string text)
     {
+        AddJavaScriptToBuffer("updateStatus", text.Replace(Environment.NewLine, "<br />"));
     }
 
     void IPlayer.SetBackground(string colour)
     {
+        // TODO
     }
 
     void IPlayer.SetForeground(string colour)
     {
+        // TODO
     }
 
     void IPlayer.SetLinkForeground(string colour)
     {
+        // TODO
     }
 
     void IPlayer.RunScript(string function, object[] parameters)
     {
+        // TODO
     }
 
     void IPlayer.Quit()
     {
+        // TODO
     }
 
     void IPlayer.SetFont(string fontName)
     {
+        // TODO
     }
 
     void IPlayer.SetFontSize(string fontSize)
     {
+        // TODO
     }
 
     void IPlayer.Speak(string text)
     {
+        // TODO
     }
 
     void IPlayer.RequestSave(string html)
     {
+        // TODO
     }
 
     void IPlayer.Show(string element)
     {
+        DoShowHide(element, true);
     }
 
     void IPlayer.Hide(string element)
     {
+        DoShowHide(element, false);
+    }
+    
+    private static readonly Dictionary<string, string> ElementMap = new()
+    {
+        { "Panes", "#gamePanes" },
+        { "Location", "#location" },
+        { "Command", "#txtCommandDiv" }
+    };
+
+    private static string? GetElementId(string code)
+    {
+        ElementMap.TryGetValue(code, out var id);
+        return id;
+    }
+    
+    private void DoShowHide(string element, bool show)
+    {
+        var jsElement = GetElementId(element);
+        if (string.IsNullOrEmpty(jsElement)) return;
+        AddJavaScriptToBuffer(show ? "uiShow" : "uiHide", jsElement);
     }
 
     void IPlayer.SetCompassDirections(IEnumerable<string> dirs)
     {
+        // TODO
     }
 
     void IPlayer.SetInterfaceString(string name, string text)
     {
+        // TODO
     }
 
     void IPlayer.SetPanelContents(string html)
     {
+        // TODO
     }
 
     void IPlayer.Log(string text)
     {
+        // TODO
     }
 
     string IPlayer.GetUIOption(UIOption option)
