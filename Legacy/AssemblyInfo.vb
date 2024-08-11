e2ef6456c468d67cb8054db536208f80d07e41ac
Alex Warren <alex@alexwarren.uk>
2024-08-11 13:59:20 +0100

    Delete VB implementation

--- Legacy/AssemblyInfo.vb
+++
@@ -1,9 +0,0 @@
-Imports System.Reflection
-Imports System.Runtime.CompilerServices
-Imports System.Runtime.InteropServices
-<Assembly: AssemblyTrademark("")>
-<Assembly: AssemblyCulture("")>
-
-
-
-<Assembly: ComVisibleAttribute(False)> 
