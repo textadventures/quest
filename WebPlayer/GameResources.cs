d1f0af1f2ae928eada742a4084d9fc07421a629d
Alex Warren <alex@alexwarren.uk>
2024-08-11 18:31:45 +0100

    Workaround for ZipArchive's lack of thread safety

--- WebPlayer/GameResources.cs
+++ WebPlayer/GameResources.cs
@@ -15,15 +15,22 @@ public static class GameResources
         ResourceProviders[Key(gameProvider, gameId)] = resourceProvider;
     }
     
+    private static readonly object ResourceStreamLock = new();
+    
     public static IResult GetResource(string provider, string id, string name)
     {
         var key = Key(provider, id);
         if (!ResourceProviders.TryGetValue(key, out var resourceProvider)) return Results.StatusCode(StatusCodes.Status404NotFound);
+
+        // Workaround for ZipArchive not being thread-safe - if multiple requests are made simultaneously for resources
+        // in the same zip file, some of those requests can fail.
+        lock (ResourceStreamLock)
+        {
+            var stream = resourceProvider(name);
+            if (stream == null) return Results.StatusCode(StatusCodes.Status404NotFound);
         
-        var stream = resourceProvider(name);
-        if (stream == null) return Results.StatusCode(StatusCodes.Status404NotFound);
-        
-        new FileExtensionContentTypeProvider().TryGetContentType(name, out var contentType);
-        return Results.Stream(stream, contentType);
+            new FileExtensionContentTypeProvider().TryGetContentType(name, out var contentType);
+            return Results.Stream(stream, contentType);    
+        }
     }
 }
