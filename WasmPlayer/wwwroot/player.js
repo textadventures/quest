4807c05c0a481e5e30f2656cc47c22324c10ac24
Alex Warren <alex@alexwarren.uk>
2024-08-17 15:04:07 +0100

    Copy resources to wwwroot

---
+++ WasmPlayer/wwwroot/player.js
@@ -0,0 +1,262 @@
+var $_GET = {};
+var webPlayer = true;
+var tmrTick = null;
+var tickCount = 0;
+var sendNextGameTickerAfter = 0;
+var canSendCommand = true;
+var apiRoot = "http://textadventures.co.uk/";
+var outputBufferId;
+var gameSessionLogData;
+
+$(() => {
+    canSendCommand = true;
+    
+    // TODO: Play.aspx used this:
+    // init('<%= ApiRoot() %>', '<%= GameSessionLogId() %>')
+    
+    init(null, null);
+});
+
+document.location.search.replace(/\??(?:([^=]+)=([^&]*)&?)/g, function () {
+    function decode(s) {
+        return decodeURIComponent(s.split("+").join(" "));
+    }
+
+    $_GET[decode(arguments[1])] = decode(arguments[2]);
+});
+
+function init(url, gameSessionLogId) {
+    apiRoot = url;
+    $("#jquery_jplayer").jPlayer({ supplied: "wav, mp3" });
+
+    // TODO: Temporarily always showing Save button here - need to work out where the game gets
+    // saved (localStorage?), and if we implement server-side saving below
+    $("#cmdSave").show();
+
+    if (apiRoot) {
+        $.ajax({
+            url: apiRoot + "games/cansave",
+            success: function (result) {
+                if (result) {
+                    $("#cmdSave").show();
+                }
+            },
+            xhrFields: {
+                withCredentials: true
+            }
+        });   
+    }
+
+    if (gameSessionLogId) {
+        $.ajax({
+            url: apiRoot + "games/startsession/?gameId=" + $_GET["id"] + "&blobId=" + gameSessionLogId,
+            success: function (result) {
+                if (result) {
+                    gameSessionLogData = result;
+                    setUpSessionLog();
+                }
+            },
+            type: "POST",
+            xhrFields: {
+                withCredentials: true
+            }
+        });
+    }
+}
+
+function setOutputBufferId(id) {
+    outputBufferId = id;
+    setUpSessionLog();
+}
+
+function setUpSessionLog() {
+    if (outputBufferId && gameSessionLogData) {
+        $.post("/GameSession/Init/?userId=" + gameSessionLogData.UserId + "&sessionId=" + gameSessionLogData.SessionId + "&bufferId=" + outputBufferId);
+    }
+}
+
+var _waitingForSoundToFinish = false;
+
+function msgboxSubmit(response) {
+    $("#msgbox").dialog("close");
+    window.setTimeout(async function () {
+        await WebPlayer.uiSetQuestionResponse(response);
+    }, 100);
+}
+
+var _menuSelection = "";
+
+function showMenu(title, options, allowCancel) {
+    $("#dialogOptions").empty();
+    $.each(options, function (key, value) {
+        $("#dialogOptions").append(
+            $("<option/>").attr("value", key).text(value)
+        );
+    });
+
+    $("#dialogCaption").html(title);
+
+    var dialogOptions = {
+        modal: true,
+        autoOpen: false,
+        buttons: [{
+            text: "Select",
+            click: function () { dialogSelect(); }
+        }]
+    };
+
+    if (allowCancel) {
+        dialogOptions.buttons = dialogOptions.buttons.concat([{
+            text: "Cancel",
+            click: function () { dialogCancel(); }
+        }]);
+        dialogOptions.close = function (event, ui) { dialogClose(); };
+    }
+    else {
+        dialogOptions.closeOnEscape = false;
+        dialogOptions.open = function (event, ui) { $(".ui-dialog-titlebar-close").hide(); };    // suppresses "close" button
+    }
+
+    _menuSelection = "";
+    $("#dialog").dialog(dialogOptions);
+
+    $("#dialog").dialog("open");
+}
+
+function dialogSelect() {
+    _menuSelection = $("#dialogOptions").val();
+    if (_menuSelection.length > 0) {
+        $("#dialog").dialog("close");
+        window.setTimeout(function () {
+            WebPlayer.uiChoice(_menuSelection);
+        }, 100);
+    }
+}
+
+function dialogCancel() {
+    $("#dialog").dialog("close");
+}
+
+function dialogClose() {
+    if (_menuSelection.length == 0) {
+        dialogSendCancel();
+    }
+}
+
+function dialogSendCancel() {
+    window.setTimeout(async function () {
+        await WebPlayer.uiChoiceCancel();
+    }, 100);
+}
+
+function sendCommand(text, metadata) {
+    if (_pauseMode || _waitingForSoundToFinish || _waitMode || !canSendCommand) return;
+    canSendCommand = false;
+    markScrollPosition();
+    
+    // TODO: See if setTimeout is still needed here
+    window.setTimeout(async function () {
+        // TODO: Check metadata format
+        await WebPlayer.sendCommand(text, getTickCountAndStopTimer(), {
+            command: text,
+            metadata
+        });
+    }, 100);
+}
+
+function ASLEvent(event, parameter) {
+    if (!canSendCommand) return false;
+    canSendCommand = false;
+    // using setTimeout here to work around a double-submission race condition which seems to only affect Firefox,
+    // even though we use "return false" to suppress submission of the form with the Enter key.
+    window.setTimeout(async function () {
+        await WebPlayer.uiSendEvent(event, parameter);
+    }, 100);
+    return true;
+}
+
+function disableMainScrollbar() {
+    $("#divOutput").css("overflow", "hidden");
+}
+
+var _timerRunning = false;
+
+function startTimer() {
+    if (_timerRunning) return;
+    _timerRunning = true;
+    tickCount = 0;
+    tmrTick = setInterval(async function () {
+        await timerTick();
+    }, 1000);
+}
+
+function stopTimer() {
+    if (!_timerRunning) return;
+    _timerRunning = false;
+    clearInterval(tmrTick);
+}
+
+async function timerTick() {
+    tickCount++;
+    if (sendNextGameTickerAfter > 0 && tickCount >= sendNextGameTickerAfter) {
+        await WebPlayer.uiTick(getTickCountAndStopTimer());
+    }
+}
+
+function getTickCountAndStopTimer() {
+    stopTimer();
+    return tickCount;
+}
+
+function requestNextTimerTick(seconds) {
+    sendNextGameTickerAfter = seconds;
+    startTimer();
+}
+
+function goUrl(href) {
+    window.open(href);
+}
+
+function saveGame() {
+    window.setTimeout(async function () {
+        const saveData = $("#divOutput").html();
+        await WebPlayer.uiSaveGame(saveData);
+    }, 100);
+}
+
+function saveGameResponse(data) {
+    addText("Saving game...<br/>");
+    if (apiRoot) {
+        $.ajax({
+            url: apiRoot + "games/save/?id=" + $_GET["id"],
+            success: function (result) {
+                if (result.Success) {
+                    addText("Game saved successfully.<br/>");
+                } else {
+                    addText("Failed to save game: " + result.Reason + "<br/>");
+                }
+            },
+            error: function (xhr, status, err) {
+                console.log(status);
+                console.log(err);
+                addText("Failed to save game.<br/>");
+            },
+            xhrFields: {
+                withCredentials: true
+            },
+            type: "POST",
+            data: {
+                data: data
+            }
+        });
+    }
+    else {
+        console.log("TODO: Save game", data);
+    }
+}
+
+function addExternalScript(url) {
+    const script = document.createElement("script");
+    script.src = url;
+    document.head.appendChild(script);
+}
---
+++ WasmPlayer/wwwroot/res/grid.js
@@ -0,0 +1,448 @@
+var scale, gridX, gridY, player,
+    playerVector, playerDestination,
+    offsetVector, offsetDestination,
+    allPaths = [],
+    customLayerPaths = [],
+    customLayerObjects = {},
+    customLayerSvg = {},
+    customLayerImages = {},
+    layers = [],
+    maxLayer = 7,
+    currentLayer = 0,
+    offset = new Point(0, 0),
+    symbols = {},
+    newShapePoints = [];
+
+for (var i = -maxLayer; i <= maxLayer; i++) {
+    var layer = new Layer();
+    layers.push(project.activeLayer);
+}
+
+var customLayer = new Layer(),
+    customLayerOffset = new Point(0, 0);
+
+customLayer.visible = false;
+
+function activateLayer(index) {
+    showCustomLayer(false);
+    layers[getLayerIndex(index)].activate();
+    layers[getLayerIndex(index)].opacity = 1;
+    if (currentLayer != index) {
+        layers[getLayerIndex(currentLayer)].opacity = 0.2;
+        currentLayer = index;
+    }
+}
+
+function getLayerIndex(index) {
+    if (index < -maxLayer || index > maxLayer) {
+        alert("Layer out of bounds. Current layer range: -" + maxLayer + " to " + maxLayer);
+    }
+    // layers array represents z-indexes from -maxLayer to maxLayer
+    return index + maxLayer;
+}
+
+activateLayer(currentLayer);
+
+gridApi.setScale = function(newScale) {
+    scale = newScale;
+    gridX = new Point(scale, 0);
+    gridY = new Point(0, scale);
+};
+
+gridApi.setZoom = function(zoom) {
+    paper.view.zoom = zoom;
+};
+
+gridApi.zoomIn = function(amount) {
+    zoom = paper.view.zoom * (Math.pow(1.1, amount));
+    if (zoom > 0.0001) {
+        paper.view.zoom = zoom;
+    }
+};
+
+function onMouseDrag(event) {
+    updateOffset(event.delta);
+}
+
+
+function onMouseUp(event) {
+    if (_respondToGridClicks) {
+        var x = getGridSquareX(event.point);
+        var y = getGridSquareY(event.point);
+        ASLEvent("JS_GridSquareClick", x + ";" + y);
+    }
+}
+
+function updateOffset(delta) {
+    setOffset(getOffset() + delta);
+    var paths;
+    if (project.activeLayer == customLayer) {
+        paths = customLayerPaths;
+    }
+    else {
+        paths = allPaths;
+    }
+    for (var i = 0; i < paths.length; i++) {
+        paths[i].position += delta;
+    }
+    if (playerDestination && project.activeLayer != customLayer) {
+        playerDestination += delta;
+    }
+}
+
+function getOffset() {
+    if (project.activeLayer == customLayer) {
+        return customLayerOffset;
+    }
+    return offset;
+}
+
+function setOffset(value) {
+    if (project.activeLayer == customLayer) {
+        customLayerOffset = value;
+    }
+    else {
+        offset = value;
+    }
+}
+
+function onFrame(event) {
+    if (playerVector) {
+        var distance = player.position - playerDestination;
+        if (distance.length > playerVector.length) {
+            player.position += playerVector;
+        }
+        else {
+            player.position = playerDestination;
+            playerVector = null;
+            playerDestination = null;
+
+            var playerPositionAbsolute = player.position - offset;
+            offsetDestination = paper.view.center - playerPositionAbsolute;
+
+            offsetVector = (offsetDestination-offset) / 10;
+        }
+    }
+    if (offsetVector) {
+        var distance = offset - offsetDestination;
+        if (distance.length > offsetVector.length) {
+            updateOffset(offsetVector);
+        }
+        else {
+            updateOffset(offsetDestination-offset);
+            offsetVector = null;
+            offsetDestination = null;
+        }
+    }
+}
+
+gridApi.drawGrid = function(minX, minY, maxX, maxY, border) {
+
+    function gridLine(start, end) {
+        var path = new Path();
+        path.strokeColor = border;
+        path.add(start, end);
+        addPathToCurrentLayerList(path);
+    }
+
+    // draw the vertical lines
+    for (var x = minX; x <= maxX; x++) {
+        var start = gridPoint(x, minY);
+        var end = gridPoint(x, maxY);
+        gridLine(start, end);
+    }
+
+    // draw the horizontal lines
+    for (var y = minY; y <= maxY; y++) {
+        var start = gridPoint(minX, y);
+        var end = gridPoint(maxX, y);
+        gridLine(start, end);
+    }
+};
+
+function gridPoint(x, y) {
+    return (gridX * x) + (gridY * y) + getOffset();
+}
+
+function getGridSquareX(point) {
+    return Math.floor(((point - getOffset()) / gridX).x);
+}
+
+function getGridSquareY(point) {
+    return Math.floor(((point - getOffset()) / gridY).y);
+}
+
+function gridPointNudge(x, y, nudgeX, nudgeY) {
+    var result = gridPoint(x, y);
+    result.x += nudgeX;
+    result.y += nudgeY;
+    return result;
+}
+
+var firstBox = true;
+
+gridApi.drawBox = function(x, y, z, width, height, border, borderWidth, fill, sides) {
+    activateLayer(z);
+    // if this is the very first room, centre the canvas by updating the offset
+    if (firstBox) {
+        var centrePoint = gridPoint(x + width / 2, y + height / 2);
+        var offsetX = paper.view.center.x - centrePoint.x;
+        var offsetY = paper.view.center.y - centrePoint.y;
+        updateOffset(new Point(offsetX, offsetY));
+        firstBox = false;
+    }
+    var path = null;
+    var points = [gridPoint(x, y), gridPoint(x + width, y), gridPoint(x + width, y + height), gridPoint(x, y + height)];
+    // sides is encoded with bits to represent NESW
+    var draw = [sides & 8, sides & 4, sides & 2, sides & 1];
+    for (var i = 0; i < 4; i++) {
+        var next = (i + 1) % 4;
+        if (draw[i]) {
+            if (path == null) {
+                path = new Path();
+                allPaths.push(path);
+                if (borderWidth > 0) {
+                    path.strokeColor = border;
+                    path.strokeWidth = borderWidth;
+                }
+                path.add(points[i]);
+            }
+            path.add(points[next]);
+        } else {
+            path = null;
+        }
+    }
+    var fillPath;
+    if (sides == 15) {
+        fillPath = path;
+    } else {
+        fillPath = new Path();
+        fillPath.add(points[0], points[1], points[2], points[3]);
+        allPaths.push(fillPath);
+    }
+    fillPath.fillColor = fill;
+    fillPath.closed = true;
+};
+
+gridApi.drawLine = function(x1, y1, x2, y2, border, borderWidth) {
+    var path = new Path;
+    path.strokeColor = border;
+    path.strokeWidth = borderWidth;
+    path.add(gridPoint(x1, y1));
+    path.add(gridPoint(x2, y2));
+    addPathToCurrentLayerList(path);
+};
+
+gridApi.drawArrow = function (id, x1, y1, x2, y2, border, borderWidth) {
+    clearExistingObject(id);
+    
+    var linePath = new Path;
+    var start = gridPoint(x1, y1);
+    var end = gridPoint(x2, y2);
+    linePath.strokeColor = border;
+    linePath.strokeWidth = borderWidth;
+    linePath.add(start);
+    linePath.add(end);
+    addPathToCurrentLayerList(linePath);
+
+    var vector = end - start;
+    var arrowVector = vector.normalize(10);
+    var arrowheadPath = new Path([
+        end + arrowVector.rotate(150),
+        end,
+        end + arrowVector.rotate(-150)
+    ]);
+    arrowheadPath.strokeColor = border;
+    arrowheadPath.strokeWidth = borderWidth;
+    addPathToCurrentLayerList(arrowheadPath);
+
+    customLayerObjects[id] = [linePath, arrowheadPath];
+};
+
+function addPathToCurrentLayerList(path) {
+    if (project.activeLayer == customLayer) {
+        customLayerPaths.push(path);
+    }
+    else {
+        allPaths.push(path);
+    }
+}
+
+gridApi.drawPlayer = function(x, y, z, radius, border, borderWidth, fill) {
+    activateLayer(z);
+    if (!player) {
+        player = new Path.Circle(gridPoint(x, y), radius);
+        player.strokeColor = border;
+        player.strokeWidth = borderWidth;
+        player.fillColor = fill;
+        player.fillColor = fill;
+        allPaths.push(player);
+
+        var playerPositionAbsolute = player.position - offset;
+        var offsetDestinationX = paper.view.center.x - playerPositionAbsolute.x;
+        var offsetDestinationY = paper.view.center.y - playerPositionAbsolute.y;
+
+        offsetDestination = new Point(offsetDestinationX, offsetDestinationY);
+        offsetVector = (offsetDestination - offset);
+    } else {
+        playerDestination = gridPoint(x, y);
+        playerVector = (playerDestination - player.position) / 10;
+        // move player to the end of the activeLayer so it gets drawn on top
+        project.activeLayer.addChild(player);
+    }
+};
+
+ 
+gridApi.drawLabel = function(x, y, z, text, col) {
+    if (col === undefined) col = "black";
+    activateLayer(z);
+    var pointText = new PointText(gridPoint(x, y));
+    pointText.justification = "center";
+    pointText.fillColor = col;
+    pointText.content = text;
+    allPaths.push(pointText);
+};
+
+function showCustomLayer(visible) {
+    if (visible != customLayer.visible) {
+        customLayer.visible = visible;
+        for (var idx = 0; idx < layers.length; idx++) {
+            layers[idx].visible = !visible;
+        }
+        if (visible) {
+            customLayer.activate();
+        }
+        else {
+            layers[getLayerIndex(currentLayer)].activate();
+        }
+    }
+}
+
+gridApi.showCustomLayer = function(visible) {
+    showCustomLayer(visible);
+};
+
+gridApi.clearCustomLayer = function() {
+    customLayer.removeChildren();
+};
+
+gridApi.clearAllLayers = function () {
+    player = null;
+    $.each(layers, function(idx, layer) {
+        layer.removeChildren();
+    });
+};
+
+gridApi.setCentre = function(x, y) {
+    var centrePoint = gridPoint(x, y);
+    var offsetX = paper.view.center.x - centrePoint.x;
+    var offsetY = paper.view.center.y - centrePoint.y;
+    var curOffset = getOffset();
+    updateOffset(new Point(offsetX, offsetY));
+};
+
+gridApi.drawCustomLayerSquare = function(id, x, y, width, height, text, fill) {
+    var points = [];
+    points.push(gridPointNudge(x, y, 1, 1));
+    points.push(gridPointNudge(x + width, y, -1, 1));
+    points.push(gridPointNudge(x + width, y + height, -1, -1));
+    points.push(gridPointNudge(x, y + height, 1, -1));
+
+    var textPoint = gridPoint(x + width / 2, y + height / 2);
+    gridApi.drawCustomLayerObject(id, points, text, textPoint, fill, fill);
+};
+
+function clearExistingObject(id) {
+    var existing = customLayerObjects[id];
+    if (existing) {
+        for (var idx in existing) {
+            var path = existing[idx];
+            // TO DO: Should remove path from layer and layerlist array
+            path.visible = false;
+        }
+    }
+}
+
+gridApi.drawCustomLayerObject = function (id, points, text, textPoint, border, fill, opacity) {
+    clearExistingObject(id);
+
+    var paths = new Array();
+    path = new Path();
+    path.strokeColor = border;
+    $.each(points, function(index, value) {
+        path.add(value);
+    });
+    path.fillColor = fill;
+    path.closed = true;
+    if (typeof opacity != "undefined") {
+        path.opacity = opacity;
+    }
+    addPathToCurrentLayerList(path);
+    paths.push(path);
+
+    if (text) {
+        var pointText = new PointText(textPoint);
+        pointText.justification = "center";
+        pointText.fillColor = "black";
+        pointText.content = text;
+        if (typeof opacity != "undefined") {
+            pointText.opacity = opacity;
+        }
+        addPathToCurrentLayerList(pointText);
+        paths.push(pointText);
+    }
+
+    customLayerObjects[id] = paths;
+};
+
+gridApi.loadSvg = function (data, id) {
+    var svg = paper.project.importSVG(data);
+    if (svg) {
+        symbols[id] = new Symbol(svg);
+    }
+};
+
+gridApi.drawCustomLayerSvg = function (id, symbolId, x, y, width, height) {
+    if (symbolId in symbols) {
+        var existing = customLayerSvg[id];
+        var placedSymbol = existing ? existing : symbols[symbolId].place();
+        placedSymbol.scale(gridX.x * width / placedSymbol.bounds.width, gridY.y * height / placedSymbol.bounds.height);
+        placedSymbol.position = gridPoint(x, y) + placedSymbol.bounds.size / 2;
+        if (!existing) addPathToCurrentLayerList(placedSymbol);
+        customLayerSvg[id] = placedSymbol;
+    } else {
+        console.log("No symbol loaded with id '" + symbolId + "'");
+    }
+};
+
+gridApi.drawCustomLayerImage = function(id, url, x, y, width, height) {
+    var existing = customLayerImages[id];
+    var raster = existing ? existing : new Raster(url);
+    var resizeRaster = function() {
+        raster.scale(gridX.x * width / raster.bounds.width, gridY.y * height / raster.bounds.height);
+        raster.position = gridPoint(x, y) + raster.bounds.size / 2;
+    };
+    if (existing) {
+        resizeRaster();
+    } else {
+        raster.onLoad = resizeRaster;
+        addPathToCurrentLayerList(raster);
+        customLayerImages[id] = raster;
+    }    
+}
+
+gridApi.addNewShapePoint = function (x, y) {
+    newShapePoints.push([x, y]);
+};
+
+gridApi.drawShape = function (id, border, fill, opacity) {
+    var points = [];
+    for (var idx in newShapePoints) {
+        var xy = newShapePoints[idx];
+        points.push(gridPoint(xy[0], xy[1]));
+    }
+    gridApi.drawCustomLayerObject(id, points, null, null, border, fill, opacity);
+    newShapePoints = [];
+};
+
+gridApi.onLoad();
