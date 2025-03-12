dc985c3e7173b618e0d9ba8f9e5f4dcec563898b
Alex Warren <alex@alexwarren.uk>
2025-03-12 08:02:21 +0000

    Add Discord and Edit links

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -4,22 +4,29 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [
-        starlight({
-            title: 'Quest Viva',
-            social: {
-                github: 'https://github.com/textadventures/quest',
-            },
-            sidebar: [
-                {
-                    label: 'Guides',
-                    autogenerate: { directory: 'guides' },
-                },
-                {
-                    label: 'Project',
-                    autogenerate: { directory: 'project' },
-                },
-            ],
-        }),
-    ],
+  integrations: [
+    starlight({
+      title: 'Quest Viva',
+      logo: {
+        src: './src/assets/quest-viva.svg',
+      },
+      social: {
+        github: 'https://github.com/textadventures/quest',
+        discord: 'https://textadventures.co.uk/community/discord',
+      },
+      editLink: {
+        baseUrl: 'https://github.com/textadventures/quest/edit/main/site/',
+      },
+      sidebar: [
+        {
+          label: 'Guides',
+          autogenerate: { directory: 'guides' },
+        },
+        {
+          label: 'Project',
+          autogenerate: { directory: 'project' },
+        },
+      ],
+    }),
+  ],
 });
