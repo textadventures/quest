5de7b4e63f82725648485b0369cf475853473dce
Alex Warren <alex@alexwarren.uk>
2025-03-12 07:43:58 +0000

    Add icon

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -4,22 +4,22 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'Quest Viva',
-			social: {
-				github: 'https://github.com/textadventures/quest',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					autogenerate: { directory: 'guides' },
-				},
-				{
-					label: 'Project',
-					autogenerate: { directory: 'project' },
-				},
-			],
-		}),
-	],
+    integrations: [
+        starlight({
+            title: 'Quest Viva',
+            social: {
+                github: 'https://github.com/textadventures/quest',
+            },
+            sidebar: [
+                {
+                    label: 'Guides',
+                    autogenerate: { directory: 'guides' },
+                },
+                {
+                    label: 'Project',
+                    autogenerate: { directory: 'project' },
+                },
+            ],
+        }),
+    ],
 });
