882d724e80304e374feca7cf174cccd9b4bcda6c
Alex Warren <alex@alexwarren.uk>
2025-07-10 18:43:38 +0100

    Use TLS 1.2 in WebEditor

--- WebEditor/WebEditor/Global.asax.cs
+++ WebEditor/WebEditor/Global.asax.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
@@ -16,6 +17,8 @@ namespace WebEditor
         {
             filters.Add(new ExceptionFilter());
             filters.Add(new HandleErrorAttribute());
+
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
         }
 
         public static void RegisterRoutes(RouteCollection routes)
