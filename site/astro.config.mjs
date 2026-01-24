17c1b686496bdade5654c1ddbf8059dcad15182d
Alex Warren <alex@alexwarren.uk>
2026-01-24 18:45:55 +0000

    Site: Update astro, add linter (#1637)

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -1,32 +1,32 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    starlight({
-      title: 'Quest Viva',
-      logo: {
-        src: './src/assets/quest-viva.svg',
-      },
-      social: {
-        github: 'https://github.com/textadventures/quest',
-        discord: 'https://textadventures.co.uk/community/discord',
-      },
-      editLink: {
-        baseUrl: 'https://github.com/textadventures/quest/edit/main/site/',
-      },
-      sidebar: [
-        {
-          label: 'Guides',
-          autogenerate: { directory: 'guides' },
-        },
-        {
-          label: 'Project',
-          autogenerate: { directory: 'project' },
-        },
-      ],
-    }),
-  ],
+    integrations: [
+        starlight({
+            title: "Quest Viva",
+            logo: {
+                src: "./src/assets/quest-viva.svg",
+            },
+            social: [
+                { icon: "github", label: "GitHub", href: "https://github.com/textadventures/quest" },
+                { icon: "discord", label: "Discord", href: "https://textadventures.co.uk/community/discord" },
+            ],
+            editLink: {
+                baseUrl: "https://github.com/textadventures/quest/edit/main/site/",
+            },
+            sidebar: [
+                {
+                    label: "Guides",
+                    autogenerate: { directory: "guides" },
+                },
+                {
+                    label: "Project",
+                    autogenerate: { directory: "project" },
+                },
+            ],
+        }),
+    ],
 });
