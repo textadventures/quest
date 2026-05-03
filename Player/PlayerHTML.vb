2607f0bdaab19d773d6839264b359d1af4599f64
Alex Warren <alex@alexwarren.uk>
2026-05-03 10:30:20 +0100

    Update to CEF v147

--- Player/PlayerHTML.vb
+++ Player/PlayerHTML.vb
@@ -70,7 +70,7 @@ Public Class PlayerHTML
         ctlWebView.KeyboardHandler = m_keyHandler
 
         m_interop = New QuestCefInterop()
-        ctlWebView.JavascriptObjectRepository.Register("questCefInterop", m_interop, False)
+        ctlWebView.JavascriptObjectRepository.Register("questCefInterop", m_interop, True)
     End Sub
 
     Private Sub PlayerHTML_Disposed(sender As Object, e As EventArgs) Handles Me.Disposed
